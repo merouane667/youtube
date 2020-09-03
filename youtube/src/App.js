3cdcd5d27a56533b938faf248055d1ae2152b877
3endhazin <fyjtlmvmmml64@gmail.com>
2020-09-03 16:02:39 +0100

    salma

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -3,12 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Datacenter} from './Datacenter/Datacenter';
 import { BrowserRouter as Router , Route,Switch } from 'react-router-dom';
 import youtube from './apis/youtube'
-<<<<<<< HEAD
 import text from './Components/text'
-=======
 import Home from './Components/Home';
 
->>>>>>> dff927cd947b59eed2fb553e5e0a25ec1d253b5a
 
 function App() {
   
@@ -16,11 +13,7 @@ function App() {
     <div>
       <Datacenter>
       <Router>
-<<<<<<< HEAD
-<Route exact path="/" component={text}/>
-=======
         <Route exact path='/' component={Home} />
->>>>>>> dff927cd947b59eed2fb553e5e0a25ec1d253b5a
       </Router>
       </Datacenter>
     </div>
